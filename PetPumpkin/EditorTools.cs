6ee814de09a2e97edb3bdd4baf3273c8b5bee550
Pet <scottnicholson.j@gmail.com>
2022-02-07 16:51:45 +0100

    structure change

--- PetPumpkin/EditorTools.cs
+++ PetPumpkin/EditorTools.cs
@@ -8,6 +8,12 @@ namespace PetPumpkin
         [MenuItem("Tools/Setup/Open Script Template Directory")]
         public static void OpenScriptTemplateDirectory(){
             System.Diagnostics.Process.Start("explorer.exe", "/select,"+"C:\\Program Files\\Unity\\Hub\\Editor\\"+Application.unityVersion+"\\Editor\\Data\\Resources\\ScriptTemplates\\");
+            Application.OpenURL("https://github.com/PetPumpkin/Unity-Tools/blob/9c73a0fa517a997990b972177365957aee6b1a9c/PetPumpkin/Setup/new_script_template.txt");
+        }
+        
+        [MenuItem("Tools/Setup/Override Package Manifest")]
+        public static void OverridePackageManifest(){
+            // System.Diagnostics.Process.Start("explorer.exe", "/select,"+"C:\\Program Files\\Unity\\Hub\\Editor\\"+Application.unityVersion+"\\Editor\\Data\\Resources\\ScriptTemplates\\");
         }
     }
 }
