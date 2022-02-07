18922e0f58d0c46f6e675787cd41f7dfb4b084b5
Pet <scottnicholson.j@gmail.com>
2022-02-07 19:17:14 +0100

    manifest overriding

--- Pet Pumpkin Tools/EditorTools.cs
+++ Pet Pumpkin Tools/EditorTools.cs
@@ -1,19 +1,38 @@
 using UnityEditor;
 using UnityEngine;
+using System;
+using System.Net.Http;
 
 namespace PetPumpkin
 {
     public static class EditorTools
     {
+        static string scriptTemplateURL = "https://github.com/PetPumpkin/Unity-Tools/blob/c3a1079637cb9fc746ff8f9dabcb85fbe78869b6/Pet%20Pumpkin%20Tools/Setup/new_script_template.txt";
+        
         [MenuItem("Tools/Setup/Open Script Template Directory")]
         public static void OpenScriptTemplateDirectory(){
             System.Diagnostics.Process.Start("explorer.exe", "/select,"+"C:\\Program Files\\Unity\\Hub\\Editor\\"+Application.unityVersion+"\\Editor\\Data\\Resources\\ScriptTemplates\\");
-            Application.OpenURL("https://github.com/PetPumpkin/Unity-Tools/blob/9c73a0fa517a997990b972177365957aee6b1a9c/PetPumpkin/Setup/new_script_template.txt");
+            Application.OpenURL(scriptTemplateURL);
         }
-        
+    }
+
+    public static class Packages {
+ 
+        static string manfistGistURL = "https://gist.githubusercontent.com/PetPumpkin/7fece489160a2540d6a8a4fad75e12e0/raw/d61f4f28ab1def63b8199e704789139b25dae6ec/manifest.json";
+
         [MenuItem("Tools/Setup/Override Package Manifest")]
-        public static void OverridePackageManifest(){
-            // System.Diagnostics.Process.Start("explorer.exe", "/select,"+"C:\\Program Files\\Unity\\Hub\\Editor\\"+Application.unityVersion+"\\Editor\\Data\\Resources\\ScriptTemplates\\");
+        public static async void OverridePackageManifest(){
+            var contents = await GetContentsFromURL(manfistGistURL);
+            var existing = System.IO.Path.Combine(Application.dataPath, "../Packages/manifest.json");
+            System.IO.File.WriteAllText(existing, contents);
+            UnityEditor.PackageManager.Client.Resolve();
+        }
+
+        public static async System.Threading.Tasks.Task<string> GetContentsFromURL(string url){
+            using var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
         }
     }
 }
